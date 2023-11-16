e088573653f714694d61ad0895ac96f0eb8cf425
Eytan425 <eytanbenittah@gmail.com>
2023-11-16 12:56:00 +0200

    Homework_3

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -76,11 +76,60 @@ class Program
 
 
     }
+
+    public static int num_7(int[] array, int k)
+    {
+        int sum1 = 0, sum2 = 0;
+        for(int i = 0; i < k; i++)
+        {
+            sum1+=array[i];
+        }
+        for(int i = array.Length - 1; i >= k; i--)
+        {
+            sum2+=array[i];
+        }
+        if(sum1 > sum2)
+        {
+            return sum1;
+        }
+        else
+        {
+            return sum2;
+        }
+        
+    }
+    public static int num_8(int[] array, int k)
+    {
+        int sum = 0, max = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = 0;
+        }
+        if (k <= 0 || array.Length < k)
+        { 
+            return -1;
+        }
+        for(int i = 0; i < k; i++)
+        {
+            sum+=array[i];
+            if(i == k)
+            {
+                if(sum > max)
+                {
+                    max = sum;
+                }
+                i+=1;
+            }
+        }
+        return max;
+
+        
+    }
     public static void Main()
     {
-        int[] array = new int[]{1,2,3,4};
-        
-        num_5(array);
+        int[] array = new int[]{7,5,6,9,4};
+        int k = 3;
+        Console.Write(num_8(array, k));
 
         Console.ReadKey();
     }
